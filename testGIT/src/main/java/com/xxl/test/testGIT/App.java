5bfb54042ce0a4016edf9aa076276ab046edb1c8
xiexiaoliang <xiexl.jason@gmail.com>
2016-11-29 11:34:55 +0800

    初始化

---
+++ testGIT/src/main/java/com/xxl/test/testGIT/App.java
@@ -0,0 +1,13 @@
+package com.xxl.test.testGIT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
